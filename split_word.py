f47ee48e4224db2504f27efb9c70e2de2254ea51
Karl Bartel <karl@karl.berlin>
2022-12-21 14:21:59 +0100

    Code formatting

--- split_word.py
+++ split_word.py
@@ -6,7 +6,7 @@ import math
 from wikdict_compound import make_db
 
 if len(sys.argv) != 2 or len(sys.argv[1]) != 2:
-    print(f'Usage: {sys.argv[0]} 2_LETTER_COUNTRY_CODE')
+    print(f"Usage: {sys.argv[0]} 2_LETTER_COUNTRY_CODE")
     sys.exit(1)
 lang = sys.argv[1]
 
@@ -16,19 +16,23 @@ conn = sqlite3.connect(lang + "-compound.sqlite3")
 conn.row_factory = sqlite3.Row
 query_count = 0
 
+
 class NoMatch(Exception):
     pass
 
+
 def sol_score(solution):
-    return math.prod(score for part, score in solution) / len(solution)**2
+    return math.prod(score for part, score in solution) / len(solution) ** 2
+
 
 def print_query_plan(query, bindings={}):
     depth_of = {0: -1}
     result = conn.execute("EXPLAIN QUERY PLAN " + query, bindings).fetchall()
     for r in result:
-        depth = depth_of[r['parent']] + 1
-        depth_of[r['id']] = depth
-        print('|  ' * depth + r['detail'])
+        depth = depth_of[r["parent"]] + 1
+        depth_of[r["id"]] = depth
+        print("|  " * depth + r["detail"])
+
 
 # TODO:
 # - case
@@ -67,20 +71,20 @@ def split_word(word, ignore_word=None, first_part=True):
         raise NoMatch()
 
     solutions = []
-    best_score = max(r['rel_score'] for r in result)
+    best_score = max(r["rel_score"] for r in result)
     for r in result:
         # if r['rel_score'] < best_score / 4:
         #     break
-        rest = word.replace(r['other_written'].lower(), '')
+        rest = word.replace(r["other_written"].lower(), "")
         if not rest:
-            if r['affix_type'] in [None, 'suffix']:
-                solutions.append([(to_base_form(r), r['rel_score'])])
+            if r["affix_type"] in [None, "suffix"]:
+                solutions.append([(to_base_form(r), r["rel_score"])])
             continue
         try:
             splitted_rest = split_word(rest, first_part=False)
         except NoMatch:
             continue
-        solutions.append([(to_base_form(r), r['rel_score'])] + splitted_rest)
+        solutions.append([(to_base_form(r), r["rel_score"])] + splitted_rest)
 
     if not solutions:
         raise NoMatch()
@@ -93,10 +97,12 @@ def split_word(word, ignore_word=None, first_part=True):
 
 
 def normalize(part):
-    return part.replace('-', '')
+    return part.replace("-", "")
+
 
 def to_base_form(row):
-    return row['written_rep']
+    return row["written_rep"]
+
 
 # +anläggningsingenjör
 # +anmana
@@ -105,40 +111,46 @@ def to_base_form(row):
 # exit()
 
 counts: dict[str, list] = dict(total=[], found=[], passed=[], failed=[])
-with open(f'tests/wikidata/wikidata_{lang}.tsv') as f:
+with open(f"tests/wikidata/wikidata_{lang}.tsv") as f:
     grouped_by_compound = itertools.groupby(
-            (line.rstrip('\n').split('\t') for line in f.readlines()),
-            key=lambda line: line[0]
+        (line.rstrip("\n").split("\t") for line in f.readlines()),
+        key=lambda line: line[0],
     )
     for compound, (lines_for_compound) in grouped_by_compound:
-        if len(counts['total']) == 200:
+        if len(counts["total"]) == 200:
             break
-        if compound.endswith('bo'):
+        if compound.endswith("bo"):
             continue
         parts = [part for comp, part in lines_for_compound]
         if len(parts) == 1:
             continue
-        counts['total'].append(compound)
-        if '-s-' in parts:
-            parts.remove('-s-')  # ignore genetiv-s
+        counts["total"].append(compound)
+        if "-s-" in parts:
+            parts.remove("-s-")  # ignore genetiv-s
         normalized_test_parts = set(normalize(p) for p in parts)
         try:
             split = split_word(compound, ignore_word=compound)
-            normalized_split_parts = set(normalize(p) for p, score in split) - {'s'}
+            normalized_split_parts = set(normalize(p) for p, score in split) - {"s"}
             if split:
-                counts['found'].append(split)
+                counts["found"].append(split)
                 match = normalized_test_parts == normalized_split_parts
                 print(compound, parts, split, match)
-                counts['passed' if match else 'failed'].append([compound, parts, split])
+                counts["passed" if match else "failed"].append([compound, parts, split])
             else:
                 pass
         except NoMatch:
             continue
 
-print(query_count, 'queries executed (', query_count / len(counts['total']),'per compound).')
-print('Counts:')
+print(
+    query_count,
+    "queries executed (",
+    query_count / len(counts["total"]),
+    "per compound).",
+)
+print("Counts:")
 for key, val in counts.items():
-    print('\t', key, len(val))
+    print("\t", key, len(val))
+
 min_success = len(counts["passed"]) / len(counts["total"])
 max_success = len(counts["found"]) / len(counts["total"])
 print(
