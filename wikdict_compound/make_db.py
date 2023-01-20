c7d435fef82482e89a54d19443669c06772527b4
Karl Bartel <karl@karl.berlin>
2023-01-20 20:53:06 +0100

    Support turning views into tables for debugging

--- wikdict_compound/make_db.py
+++ wikdict_compound/make_db.py
@@ -3,6 +3,10 @@ from pathlib import Path
 import hashlib
 import sys
 
+
+DEBUG_DB = False
+
+
 with open(__file__, "rb") as f:
     md5sum = hashlib.md5(f.read()).hexdigest()
 
@@ -29,6 +33,10 @@ def make_db(lang: str, input_path, output_path) -> None:
             conn.close()
             outfile.unlink(missing_ok=True)
 
+    # For debugging, it is very helpful to store all intermediate results
+    temp_table = "TEMPORARY TABLE" if not DEBUG_DB else "TABLE"
+    temp_view = "TEMPORARY VIEW" if not DEBUG_DB else "TABLE"
+
     conn = sqlite3.connect(outfile, isolation_level="IMMEDIATE")
     conn.executescript(
         rf"""
@@ -36,7 +44,7 @@ def make_db(lang: str, input_path, output_path) -> None:
 
         ATTACH DATABASE '{input_path}/{lang}.sqlite3' AS generic;
 
-        CREATE TEMPORARY VIEW form_with_entry AS
+        CREATE {temp_table} form_with_entry AS
         SELECT *
         FROM generic.form
             JOIN entry USING (lexentry)
@@ -47,7 +55,7 @@ def make_db(lang: str, input_path, output_path) -> None:
             written_rep NOT LIKE '% %'
         ;
 
-        CREATE TEMPORARY VIEW terms_from_forms AS
+        CREATE {temp_view} terms_from_forms AS
         SELECT other_written, written_rep, part_of_speech,
             CASE "case"
                 WHEN 'GenitiveCase' THEN 1
@@ -56,44 +64,16 @@ def make_db(lang: str, input_path, output_path) -> None:
         FROM form_with_entry
         ;
 
-        CREATE TEMPORARY VIEW terms_from_entries AS
-        SELECT written_rep, written_rep, part_of_speech, 1 AS score_factor
+        CREATE {temp_view} terms_from_entries AS
+        SELECT written_rep AS other_written, written_rep, part_of_speech, 1 AS score_factor
         FROM generic.entry
         ;
 
-        CREATE TEMPORARY TABLE terms AS
+        CREATE {temp_table} terms AS
         SELECT * FROM terms_from_forms
         UNION ALL
         SELECT * FROM terms_from_entries
         ;
-
-        CREATE TEMPORARY VIEW terms_view AS
-        SELECT
-            written_rep,
-            other_written,
-            part_of_speech,
-            -- Affixes are not that important words in a normal
-            -- dictionary, but for in compound words, they are very likely
-            -- and should be prioritized.
-            score_factor * CASE
-                WHEN affix_type == 'suffix' THEN 3
-                WHEN affix_type IN ('infix', 'prefix') THEN 2
-                ELSE 1
-            END AS score_factor,
-            affix_type
-        FROM (
-            SELECT
-                written_rep,
-                lower(trim(other_written, '-')) AS other_written,
-                part_of_speech,
-                score_factor,
-                CASE
-                    WHEN substr(other_written, 1, 1) = '-' AND substr(other_written, -1, 1) = '-' THEN 'infix'
-                    WHEN substr(other_written, 1, 1) = '-' THEN 'suffix'
-                    WHEN substr(other_written, -1, 1) = '-' THEN 'prefix'
-                END AS affix_type
-            FROM terms
-        );
     """
     )
 
@@ -149,7 +129,52 @@ def make_db(lang: str, input_path, output_path) -> None:
         )
 
     conn.executescript(
-        """
+        f"""
+        CREATE {temp_view} terms_view AS
+        SELECT
+            written_rep,
+            other_written,
+            part_of_speech,
+            -- Affixes are not that important words in a normal
+            -- dictionary, but for in compound words, they are very likely
+            -- and should be prioritized.
+            score_factor * CASE
+                WHEN affix_type == 'suffix' THEN 3
+                WHEN affix_type IN ('infix', 'prefix') THEN 2
+                ELSE 1
+            END AS score_factor,
+            affix_type
+        FROM (
+            SELECT
+                written_rep,
+                lower(trim(other_written, '-')) AS other_written,
+                part_of_speech,
+                score_factor,
+                CASE
+                    WHEN substr(other_written, 1, 1) = '-' AND substr(other_written, -1, 1) = '-' THEN 'infix'
+                    WHEN substr(other_written, 1, 1) = '-' THEN 'suffix'
+                    WHEN substr(other_written, -1, 1) = '-' THEN 'prefix'
+                END AS affix_type
+            FROM terms
+        );
+
+        CREATE {temp_view} compound_splitter_ungrouped AS
+        SELECT
+            other_written,
+            affix_type,
+            coalesce(rel_score, 0.1) * score_factor AS rel_score,
+            written_rep,
+            part_of_speech
+        FROM terms_view
+            LEFT JOIN rel_importance ON (written_rep = written_rep_guess)
+        WHERE other_written != '' -- Why are there forms without text?
+          AND NOT (length(other_written) = 1 AND affix_type IS NULL)
+          AND (
+            part_of_speech NOT IN (
+                'interjection', 'pronoun', 'indefinitePronoun', 'proverb', 'phraseologicalUnit', 'symbol',
+                'article', 'idiom', 'properNoun')
+            );
+
         CREATE TABLE compound_splitter AS
         SELECT 
             other_written,
@@ -160,23 +185,7 @@ def make_db(lang: str, input_path, output_path) -> None:
                 PARTITION BY other_written, affix_type
                 ORDER BY rel_score DESC
             ) AS written_rep
-        FROM (
-            SELECT
-                other_written,
-                affix_type,
-                coalesce(rel_score, 0.1) * score_factor AS rel_score,
-                written_rep,
-                part_of_speech
-            FROM terms_view
-                LEFT JOIN rel_importance ON (written_rep = written_rep_guess)
-            WHERE other_written != '' -- Why are there forms without text?
-              AND NOT (length(other_written) = 1 AND affix_type IS NULL)
-              AND (
-                part_of_speech NOT IN (
-                    'interjection', 'pronoun', 'indefinitePronoun', 'proverb', 'phraseologicalUnit', 'symbol',
-                    'article', 'idiom', 'properNoun')
-                )
-        )
+        FROM compound_splitter_ungrouped
         GROUP BY 1, 2;
         
         CREATE INDEX compound_splitter_idx ON compound_splitter(other_written);
